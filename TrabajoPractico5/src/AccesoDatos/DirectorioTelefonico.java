adcdbe3b609d8e43905e5249216675cb5f48ab82
Federico Galan <tumachgalan14@gmail.com>
2025-09-08 17:26:47 -0300

    ss

--- TrabajoPractico5/src/AccesoDatos/DirectorioTelefonico.java
+++ TrabajoPractico5/src/AccesoDatos/DirectorioTelefonico.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author abate
+ * @author Federico_Galan abate
  */
 public class DirectorioTelefonico {
     private TreeMap<Long,Contacto> agenda = new TreeMap<>();
